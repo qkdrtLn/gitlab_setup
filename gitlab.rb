532a3072b21b1a04b57611ce20da1ab47b60e0a4
qkdrtLn <serkin3n1@gmail.com>
2025-10-19 18:55:31 +0200

    edit url

--- gitlab.rb
+++ gitlab.rb
@@ -1,2 +1,2 @@
-external_url 'gitlab.nikitaserkin.pp.ua'
+external_url 'https://gitlab.nikitaserkin.pp.ua/'
 gitlab_rails['initial_root_password'] = File.read('/run/secrets/gitlab_root_password').gsub("\n", "")
