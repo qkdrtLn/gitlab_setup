2404dc111f21cdb832e25f7b7b1d667b67a59d99
qkdrtLn <serkin3n1@gmail.com>
2025-10-19 18:09:51 +0200

    add-domane-config

--- gitlab.rb
+++ gitlab.rb
@@ -1,2 +1,2 @@
-external_url 'https://my.domain.com/'
+external_url 'gitlab.nikitaserkin.pp.ua'
 gitlab_rails['initial_root_password'] = File.read('/run/secrets/gitlab_root_password').gsub("\n", "")
