0053aa04ec4fd7de287c1c89c1970eac934bcccc
qkdrtLn <serkin3n1@gmail.com>
2025-11-02 17:05:07 +0100

    edit gitlab.rb

--- gitlab.rb
+++ gitlab.rb
@@ -1,2 +1,12 @@
 external_url 'https://gitlab.nikitaserkin.pp.ua/'
 gitlab_rails['initial_root_password'] = File.read('/run/secrets/gitlab_root_password').gsub("\n", "")
+nginx['enable'] = true
+nginx['listen_port'] = 80
+nginx['listen_https'] = false
+nginx['error_log_level'] = "debug"
+nginx['proxy_set_headers'] = {
+    "X-Forwarded-Proto" => "https",
+    "X-Forwarded-Ssl" => "on"
+}
+gitlab_rails['gitlab_shell_ssh_port'] = 2224
+letsencrypt['enable'] = false
